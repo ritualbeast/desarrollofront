c7f2a1ac4a917e3211c5da006073f48e18ccb6ac
zafiro <ladvdgigi@gmail.com>
2023-06-13 17:54:29 -0500

    13 de junio 17pm

--- src/views/Encuestas.jsx
+++ src/views/Encuestas.jsx
@@ -4,8 +4,43 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import { BiPlus } from 'react-icons/bi';
 import { Select, Pagination } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { makeStyles } from "@material-ui/core";
+import { Dropdown } from 'react-bootstrap';
+import svgManager from '../assets/svg';
+/*iconos menu encuestas*/
+import puntos from '../assets/img/icono_3puntos.jpg';
+import copy from '../assets/img/icon_copy.png';
+import eliminar from '../assets/img/icon_basura.png';
+import base from '../assets/img/icon_database.png';
+const pagination = makeStyles({
+  root: {
+    "& li.Mui-selected": {
+      color: "yellow",
+      backgroundColor: "yellow"
+    },
+    "& li:nth-of-type(2).Mui-selected": {
+      backgroundColor: "red"
+    }
+  }
+});
+
+const copySVG = svgManager.getSVG('copy');
+const verticalSVG = svgManager.getSVG('vertical');
+const eyeSVG = svgManager.getSVG('eye');
+const shareSVG = svgManager.getSVG('share');
+const databaseSVG = svgManager.getSVG('database');
+const sendSVG = svgManager.getSVG('send');
+const trash = svgManager.getSVG('trash');
+
+const paginationStyle = {
+  '& .Mui-selected': {
+    color: '#0a0800',
+    backgroundColor: '#f3cd4f',
+  },
+};
 
 const Encuestas = () => {
+  const paginationClass = pagination();
   return (
     <Container fluid className='encuesta-container'>
       <Row id="encuestas-Row">
@@ -38,9 +73,9 @@ const Encuestas = () => {
         <Col xs={6} className="encuestas-filtrarpor">
           <h4>Filtrar por:</h4>
           <ul className="encuestas-filtrarpor__ul">
-            <li className="encuestas-filtrarpor__li"><span>Abiertas</span></li>
-            <li className="encuestas-filtrarpor__li"><span>Cerradas</span></li>
-            <li className="encuestas-filtrarpor__li"><span>Todas</span></li>
+            <li className="encuestas-filtrarpor__li"><a href=''>Abiertas</a></li>
+            <li className="encuestas-filtrarpor__li"><a href=''>Cerradas</a></li>
+            <li className="encuestas-filtrarpor__li"><a href=''>Todas</a></li>
           </ul>
         </Col>
 
@@ -56,8 +91,40 @@ const Encuestas = () => {
       </Row>
       <Row className="encuestas-cuerpo">
         <Col xs={4} className="encuestas-cuerpo__col">
-            <h4>Encuesta 1</h4>
+        <div className='encuestas-titulo'>
+          <h4 className='encuestas-titulo__h4'>Encuesta 1</h4>
+          <Dropdown className='encuestas-dropdownmenu'>
+            <Dropdown.Toggle variant="primary" id="dropdown-menu" className='encuestas-icon'>
+              <span dangerouslySetInnerHTML={{ __html: verticalSVG }} />
+            </Dropdown.Toggle>
+            <Dropdown.Menu className='dropdown-menu-right dropdown-column'>
+              
+              <Dropdown.Item className='encuesta-item'>
+                <span dangerouslySetInnerHTML={{ __html: copySVG }} />
+                Duplicar</Dropdown.Item>
+              <Dropdown.Item className='encuesta-item'>
+                <span dangerouslySetInnerHTML={{ __html: eyeSVG }} />
+                Visualizar</Dropdown.Item>
+              <Dropdown.Item className='encuesta-item'>
+                <span dangerouslySetInnerHTML={{ __html: shareSVG }} />
+                Compartir</Dropdown.Item>
+              <Dropdown.Item className='encuesta-item'>
+                <span dangerouslySetInnerHTML={{ __html: databaseSVG }} />
+                Ver datos</Dropdown.Item>
+              <Dropdown.Item className='encuesta-item'>
+                <span dangerouslySetInnerHTML={{ __html: sendSVG }} />
+                Publicar</Dropdown.Item>
+              <Dropdown.Item className='encuesta-item'>
+                <span dangerouslySetInnerHTML={{ __html: trash }} />
+                Eliminar</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+            
+
+
             <p>Creación: 01/01/2021</p>
+            
             <Button variant="primary" className="encuestas-editarbutton">
               Editar encuesta
             </Button>
@@ -80,7 +147,14 @@ const Encuestas = () => {
 
       <Row className="encuestas-paginacion">
         <Col xs={12} className="encuestas-paginacion__col">
-          <Pagination count={10} variant="outlined" shape="rounded" />
+          <div className={paginationClass.root}>
+          <Pagination
+  count={10}
+  variant="outlined"
+  shape="rounded"
+  sx={paginationStyle}
+/>
+          </div>
         </Col>
       </Row>
 
