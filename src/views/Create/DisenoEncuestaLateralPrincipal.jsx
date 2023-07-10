1d5e017a18239b31b17a36ef524d194c81141f8c
zafiro <ladvdgigi@gmail.com>
2023-07-10 17:59:55 -0500

    10 julio 6pm

--- src/views/Create/DisenoEncuestaLateralPrincipal.jsx
+++ src/views/Create/DisenoEncuestaLateralPrincipal.jsx
@@ -1,16 +1,34 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Col, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import DisenoEncuestaLaterallogotipo from './DisenoEncuestaLateralLogotipo';
 import svgManager from '../../assets/svg';
-import '../../styles/create.css'
+import '../../styles/estilodiseno.css'
 
 const helpCircleSVG = svgManager.getSVG('help-circle');
 const xSVG = svgManager.getSVG('x');
 const infoSVG = svgManager.getSVG('info');
+const imageSVG = svgManager.getSVG('image');
+const sidebarSVG = svgManager.getSVG('sidebar');
+const italicSVG = svgManager.getSVG('italic');
+const listSVG = svgManager.getSVG('list');
+const dropletSVG = svgManager.getSVG('droplet');
+const repeatSVG = svgManager.getSVG('repeat');
 
 const DisenoEncuestaLateralPrincipal = () => {
 
     const [showBancoPreguntas, setShowBancoPreguntas] = React.useState(false);
     const [showTooltip, setShowTooltip] = React.useState(false);
+
+    const [openDisenoPrincipal, setOpenDisenoPrincipal] = React.useState(true);
+    const [openDisenoLogotipo, setOpenDisenoLogotipo] = React.useState(false);
+    
+
+    const openDisenoLogotipoHandler = () => {
+        setOpenDisenoLogotipo(true);
+        setOpenDisenoPrincipal(false);
+    };
+    
+    const [filaSeleccionada, setFilaSeleccionada] = useState(null);
     const targetRef = useRef(null);
     const handleIconClick = () => {
         setShowTooltip(false);
@@ -41,48 +59,161 @@ const DisenoEncuestaLateralPrincipal = () => {
         // Lógica para manejar el clic en las opciones desplegadas
     };
 
+    // lista de diseño
+
+    const listadiseno = [  
+        {nombre: 'Logotipo', id: 1, SSVG: imageSVG},
+        {nombre: 'Pie de página', id: 2, SSVG: sidebarSVG},
+        {nombre: 'Fuentes', id: 3 , SSVG: italicSVG},
+        {nombre: 'Disposicion', id: 4 , SSVG: listSVG},
+        {nombre: 'Fondo', id: 5 , SSVG: dropletSVG},
+        {nombre: 'Transcición', id: 6 , SSVG: repeatSVG},
+    ];
+
+    const elementosPorFila = 2;
+
+    // Dividir el arreglo en filas de dos elementos
+    const filas = [];
+    for (let i = 0; i < listadiseno.length; i += elementosPorFila) {
+      filas.push(listadiseno.slice(i, i + elementosPorFila));
+    }
+
+    // Lista de colores
+
+    const listacolores = [
+        {nombre: '1', id: 1},
+        {nombre: '2', id: 2},
+        {nombre: '3', id: 3},
+        {nombre: '4', id: 4},
+        {nombre: '5', id: 5},
+        {nombre: '6', id: 6},
+    ];
+
+    const colores = [
+        {id: 1, color: '#FF0000'}, 
+        {id: 2, color: '#0000FF'},
+        {id: 3, color: '#008000'},
+        {id: 4, color: '#FFFF00'},
+        {id: 5, color: '#800080'},
+        {id: 6, color: '#FFA500'},
+        {id: 7, color: '#FF00FF'},
+        {id: 8, color: '#A52A2A'},
+        {id: 9, color: '#808080'}
+    ];
+
+    const handleClickFila = (id) => {
+        if (filaSeleccionada === id) {
+          setFilaSeleccionada(null); // Desactivar la selección si se hace clic nuevamente en la misma fila
+        } else {
+          setFilaSeleccionada(id);
+          console.log("ok")
+        }
+      };
+
+    const handleClickElemento = (id) => {
+        if (id === 1) {
+            openDisenoLogotipoHandler();
+        }
+
+    };
+
+    
     
   return (
     <>
-        <Col className="encuesta-Segundocuerpo2">
-            <Col>
-            <div className="encuesta-subtitulo2">
-                <h2 className="encuesta-subtitulo-2">Estilo</h2>
-                <OverlayTrigger
-                trigger="click"
-                show={showTooltip}
-                target={targetRef.current}
-                placement="right"
-                delay={{ show: 250, hide: 400 }}
-                overlay={renderTooltip}
-                onHide={() => setShowTooltip(false)}
-                >
-                <div
-                    className="help-icon"
-                    onClick={() => setShowTooltip(!showTooltip)} // Alternar el estado de showTooltip al hacer clic en el ícono de ayuda
-                >
-                    <span
-                    ref={targetRef}
-                    style={{ marginLeft: '150px' }}
-                    dangerouslySetInnerHTML={{ __html: helpCircleSVG }}
-                    />
-                </div>
-                </OverlayTrigger>
-            </div>
-            </Col>
-            <Col>
-            
-                <div className="desplegado-container">
-                <div className="listaBancoPreguntas-2">
-                    <div className="fondo-lista">
-                    
-                    
+        {openDisenoPrincipal && (
+             <Col className="encuesta-Segundocuerpo2">
+                <Col>
+                <div className="encuesta-subtitulo2">
+                    <h2 className="encuesta-subtitulo-2">Estilo</h2>
+                    <OverlayTrigger
+                    trigger="click"
+                    show={showTooltip}
+                    target={targetRef.current}
+                    placement="right"
+                    delay={{ show: 250, hide: 400 }}
+                    overlay={renderTooltip}
+                    onHide={() => setShowTooltip(false)}
+                    >
+                    <div
+                        className="help-icon"
+                        onClick={() => setShowTooltip(!showTooltip)} // Alternar el estado de showTooltip al hacer clic en el ícono de ayuda
+                    >
+                        <span
+                        ref={targetRef}
+                        style={{ marginLeft: '150px' }}
+                        dangerouslySetInnerHTML={{ __html: helpCircleSVG }}
+                        />
                     </div>
+                    </OverlayTrigger>
                 </div>
-                </div>
-            
+                </Col>
+                <Col>
+                
+                    <div className="desplegado-container">
+                    <div className="listaBancoPreguntas-2">
+                        <div className="fondo-lista">
+                        {filas.map((fila, index) => (
+                            <div className="fila" key={index}>
+                                {fila.map((elemento) => (
+                                <div className="elemento" key={elemento.id}  onClick={() => handleClickElemento(elemento.id)}>
+                                    <span className='disenobar-nombre'>{elemento.nombre}</span>
+                                    <br/>
+                                    <div className="svg-container">
+                                    {/* Renderizar el SVG dentro del div con radius */}
+                                    <div className="svg-div">
+                                        {<span style={{marginTop: '7px'}} dangerouslySetInnerHTML={{ __html: elemento.SSVG }}/>}
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                                ))}
+                            </div>
+                            ))}  
+                            <div className="fila">
+                                Colores
+                            </div>
+                            <div className="contenedorfilas">
+                            {listacolores.map((colorL) => (
+                                <div
+                                className={`fila2${filaSeleccionada === colorL.id ? ' seleccionada' : ''}`}
+                                key={colorL.id}
+                                onClick={() => handleClickFila(colorL.id)}
+                                >
+                                <div className="elemento2">
+                                    {colores.map((color) => (
+                                    <div
+                                        className="elementoColores"
+                                        key={color.id}
+                                        style={{ backgroundColor: color.color }}
+                                    >
+                                        <span className='disenobar-nombre'>{color.nombre}</span>
+                                    </div>
+                                    ))}
+                                </div>
+                                </div>
+                            ))}
+                            </div>
+                            
+                            
+                    
+                        
+                        </div>
+                    </div>
+                    </div>
+                
+                </Col>
             </Col>
-        </Col>
+            
+            )}
+       
+
+        {openDisenoLogotipo && (
+            <DisenoEncuestaLaterallogotipo />
+        )    
+        }
+
+        
        
                                 
                                 
